082d2f2c73be33a9ca901d3c2e1710ffb9cdaea6
Isacao lindao <isacsouzapedroso@gmail.com>
2026-06-08 21:47:30 -0300

    Ajustes

---
+++ src/test/java/com/cademeupet/app/CademeupetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cademeupet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CademeupetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
