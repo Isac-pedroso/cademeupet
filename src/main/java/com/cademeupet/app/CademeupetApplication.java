891bca807f5126de705dbd646c8206e5215e82b1
isacao <isacsouzapedroso@gmail.com>
2026-06-10 22:55:01 -0300

    Ajustes novos

--- src/main/java/com/cademeupet/app/CademeupetApplication.java
+++ src/main/java/com/cademeupet/app/CademeupetApplication.java
@@ -11,12 +11,7 @@ public class CademeupetApplication {
 	public static void main(String[] args) {
 
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(CademeupetApplication.class, args);
 	}
 
